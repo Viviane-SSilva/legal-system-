79881c616e79e5c41db124ab5ebb05e9e6120b0f
Viviane Silva <viviane.silva1810@gmail.com>
2026-07-27 14:08:49 -0300

    feat(testing): setup jest with typescript and nodenext esm

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,24 @@
 export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: './prisma/prisma-environment-jest.js',
   verbose: true,
-  transform: {'^.+\\.tsx?$':'ts-jest'},
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          // Força o ts-jest a emitir ES Modules sem alterar o seu tsconfig principal
+          module: 'NodeNext',
+          moduleResolution: 'NodeNext',
+        },
+      },
+    ],
+  },
   moduleFileExtensions: ['js', 'json', 'ts'],
   testMatch: ['**/test/**/*.test.ts'],
 };
